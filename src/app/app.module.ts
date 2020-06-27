5d40296c8af9fb1cac93b7aa9ac52d4fea059192
Stevam Sousa <stevam.2@gmail.com>
2020-06-26 21:52:08 -0300

    Including and testing new framework to calendar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { jqxSchedulerModule } from 'jqwidgets-ng/jqxscheduler';
+import { CalendarComponent } from '../calendar/calendar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalendarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    jqxSchedulerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/calendar/calendar.module.ts
@@ -0,0 +1,17 @@
+/*import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { CalendarComponent } from './calendar.component'; 
+import { jqxSchedulerModule } from 'jqwidgets-ng/jqxscheduler';
+import { AppModule } from '../app/app.module';
+@NgModule({
+    declarations: [
+        CalendarComponent
+    ],
+    imports: [
+        BrowserModule,
+        jqxSchedulerModule
+    ],
+    providers: [],
+    bootstrap: [CalendarComponent]
+})
+export class CalenadarModule { }*/
