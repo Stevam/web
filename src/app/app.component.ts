29b2e94315a3b44008d10ed87e6f4713b4d695a3
Stevam Sousa <stevam.2@gmail.com>
2020-07-01 08:57:09 -0300

    Task - Communication between front-end and back-end

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+@Injectable()
 export class AppComponent {
+  constructor(private http: HttpClient) { }
   title = 'Web';
+  lista = [];
+
+  click() {
+    console.log("passou aqui");
+    this.list().subscribe((data: []) => { this.lista = data });
+  }
+
+  list(){
+    return this.http.get(`${environment.url}/list`);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { jqxSchedulerModule } from 'jqwidgets-ng/jqxscheduler';
 import { CalendarComponent } from '../calendar/calendar.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { CalendarComponent } from '../calendar/calendar.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     jqxSchedulerModule
   ],
